ba768130761af59a686eb32f8a1918a3b1397171
anhvuit <anhvu010197@gmail.com>
2019-10-01 23:16:40 +0700

    done UI list,detail inventory

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,9 +1,6 @@
 
-import React, { Component } from 'react';
+import React, { Component, createRef,useRef } from 'react';
 import {
-  Container,
-  Content,
-  Item,
   Input,
   DatePicker
 } from 'native-base';
@@ -11,126 +8,185 @@ import {
   Image,
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  ScrollView,
+  CheckBox,
+  TouchableOpacity
 } from 'react-native';
+import RadioButton from '../components/RadioButton'
 export default class DetailScreen extends Component {
+  inputRef = createRef();
 
   constructor(props) {
     super(props);
-    this.state = { chosenDate: new Date() };
+    this.state = { chosenDate: new Date(), price: "" };
     this.setDate = this.setDate.bind(this);
   }
   setDate(newDate) {
     this.setState({ chosenDate: newDate });
   }
-  render() {
 
-    return (
-      <View style={styles.container}>
-        <View style={styles.imageWrapper}>
-          <Image source={require('../assets/images/bitis_hunter_x_2019.jpg')}
-            style={{ height: 300, width: null, flex: 1, resizeMode: 'contain' }} />
-        </View>
-        <View style={styles.inforWrapper}>
-          <View style={styles.titleWrapper}>
-            <Text style={styles.title}>
-              Bitis Hunter 2019
-          </Text>
-            <Text style={styles.producer}>
-              Biti's
-          </Text>
-            <Text style={styles.inventoryAmount}>
-              9 trong 10 sản phẫm tồn kho
-          </Text>
+  componentDidMount() {
+    const { params } = this.props.navigation.state;
+    const inventory = params ? params.inventory : null;
+    alert("id " + inventory.id)
+  }
+
+  onCategoryCheckBox = (value) => {
+    alert("value " + value)
+  }
+
+  onPressUpdate = () => {
+    const { params } = this.props.navigation.state;
+    let inventory = params ? params.inventory : null;
+    const updateInventoryEvent = params ? params.updateInventoryEvent : null;
+    var value = this.inputRef.current.value;
+    if (value) { // if validation fails, value will be null
+      alert("price " + value)
+    }
+    alert("price " + value)
+
+
+    // inventory.price =this._price.value
+    updateInventoryEvent.emit("updateInventory", inventory)
+  }
+
+  onValuePriceChange = (price) => {
+    alert("price " + price + " refs" + this.refs.price)
+  }
+
+  refPrice = (component) => {
+    return this._price = component
+  }
+
+
+  render() {
+    const { params } = this.props.navigation.state;
+    const inventory = params ? params.inventory : null;
+
+    if (inventory) {
+      return (
+        <ScrollView style={styles.container}>
+          <View style={styles.imageWrapper}>
+            <Image source={inventory.image}
+              style={{ height: 100, width: null, flex: 1, resizeMode: 'contain', borderRadius: 50 }} />
           </View>
-          <View style={styles.priceWrapper}>
+          <View style={styles.inforWrapper}>
+            <View style={styles.titleWrapper}>
+              <Text style={styles.title}>
+                Bitis Hunter 2019
+            </Text>
+              <Text style={styles.producer}>
+                Biti's
+            </Text>
+              <Text style={styles.inventoryAmount}>
+                9 trong 10 sản phẫm tồn kho
+            </Text>
+            </View>
             <View style={styles.sectionTitle}>
               <Text style={{ fontSize: 16, color: 'black', marginRight: 10 }}>
                 Chọn mức giá
-          </Text>
+            </Text>
               <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'red' }}>
                 *
-          </Text>
+           </Text>
             </View>
-            <View style={{ flexDirection: 'row' }}>
-              <Text style={{ fontSize: 13, color: '#DEDEDE', marginRight: 10 }}>
-                Giợi ý giá
-          </Text>
-              <Text style={{ fontSize: 15, color: 'black' }}>
-                990.000
-          </Text>
+            <View style={styles.priceWrapper}>
+              <View style={{ flexDirection: 'row' }}>
+                <Text style={{ fontSize: 13, color: '#DEDEDE', marginRight: 10 }}>
+                  Gợi ý giá
+            </Text>
+                <Text style={{ fontSize: 15, color: '#DEDEDE' }}>
+                  900.000 - 1.000.000
+            </Text>
+              </View>
+              <View style={{ flexDirection: 'row' }}>
+                <Input placeholder="990.000" style={{ left: 0, width: 300 }} ref={this.inputRef}
+                  onChange={this.onValuePriceChange} />
+                <Text style={{ fontSize: 15, color: 'black', padding: 10 }}>
+                  VNĐ
+            </Text>
+              </View>
             </View>
-            <View style={{ flexDirection: 'row' }}>
-              <Input placeholder="Price" style={{ left: 0 , width: 300}} />
-              <Text style={{ fontSize: 15, color: 'black', padding:10 }}>
-                VNĐ
-          </Text>
-            </View>
-
-
-          </View>
-          <View style={styles.peridodWrapper}>
-            <View style={{ flexDirection: 'row', backgroundColor: '#F5F5F5', paddingVertical: 5, }}>
+            <View style={styles.sectionTitle}>
               <Text style={{ fontSize: 16, color: 'black', marginRight: 10 }}>
-              Thời hạn áp dụng
-          </Text>
+                Thời hạn áp dụng
+            </Text>
               <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'red' }}>
                 *
-          </Text>
+            </Text>
             </View>
-            <View style={{ flexDirection: 'row' }}>
-            <Text style = {{padding:10}}>
-                Bắt đầu
-              </Text>
-              <DatePicker
-                defaultDate={new Date(2018, 4, 4)}
-                minimumDate={new Date(2018, 1, 1)}
-                maximumDate={new Date(2018, 12, 31)}
-                locale={"en"}
-                timeZoneOffsetInMinutes={undefined}
-                modalTransparent={false}
-                animationType={"fade"}
-                androidMode={"default"}
-                placeHolderText="Select date"
-                textStyle={{ color: "green" }}
-                placeHolderTextStyle={{ color: "#d3d3d3" }}
-                onDateChange={this.setDate}
-                disabled={false}
-              />
-            </View>
-            <View style={{ flexDirection: 'row' }}>
-            <Text style = {{padding:10}}>
-                Kết thúc
-              </Text>
-              <DatePicker
-                defaultDate={new Date(2018, 4, 4)}
-                minimumDate={new Date(2018, 1, 1)}
-                maximumDate={new Date(2018, 12, 31)}
-                locale={"en"}
-                timeZoneOffsetInMinutes={undefined}
-                modalTransparent={false}
-                animationType={"fade"}
-                androidMode={"default"}
-                placeHolderText="Select date"
-                textStyle={{ color: "green" }}
-                placeHolderTextStyle={{ color: "#d3d3d3" }}
-                onDateChange={this.setDate}
-                disabled={false}
-              />
-            </View>
-            <View style = {{backgroundColor: 'red', height: 30, alignItems:'center', justifyContent:'center'}}>
-            <Text style={{color:'white'}}>
-               THAY ĐỔI GIÁ
-              </Text>
-            </View>
+            <View style={styles.peridodWrapper}>
 
+              <View style={{ flexDirection: 'row' }}>
+                <Text style={{ padding: 10 }}>
+                  Bắt đầu
+                </Text>
+                <DatePicker
+                  defaultDate={new Date(2018, 4, 4)}
+                  minimumDate={new Date(2018, 1, 1)}
+                  maximumDate={new Date(2018, 12, 31)}
+                  locale={"en"}
+                  timeZoneOffsetInMinutes={undefined}
+                  modalTransparent={false}
+                  animationType={"fade"}
+                  androidMode={"default"}
+                  placeHolderText="Select date"
+                  textStyle={{ color: "green" }}
+                  placeHolderTextStyle={{ color: "#d3d3d3" }}
+                  onDateChange={this.setDate}
+                  disabled={false}
+                />
+              </View>
+              <View style={{ flexDirection: 'row' }}>
+                <Text style={{ padding: 10 }}>
+                  Kết thúc
+                </Text>
+                <DatePicker
+                  defaultDate={new Date(2018, 4, 4)}
+                  minimumDate={new Date(2018, 1, 1)}
+                  maximumDate={new Date(2018, 12, 31)}
+                  locale={"en"}
+                  timeZoneOffsetInMinutes={undefined}
+                  modalTransparent={false}
+                  animationType={"fade"}
+                  androidMode={"default"}
+                  placeHolderText="Select date"
+                  textStyle={{ color: "green" }}
+                  placeHolderTextStyle={{ color: "#d3d3d3" }}
+                  onDateChange={this.setDate}
+                  disabled={false}
+                />
+              </View>
 
+              <View style={{ flexDirection: 'row', alignItems: "center" }}>
+                <CheckBox
+                  value={true}
+                  onValueChange={this.onCategoryCheckBox}
+                >
+                </CheckBox>
+                <Text>
+                  Đến khi hết hàng
+                </Text>
+              </View>
+              <TouchableOpacity style={{ backgroundColor: 'red', height: 30, alignItems: 'center', justifyContent: 'center', borderRadius: 15 }}
+                onPress={this.onPressUpdate}
+              >
+                <Text style={{ color: 'white' }}>
+                  ĐỔI GIÁ
+                </Text>
+              </TouchableOpacity>
+
+            </View>
 
           </View>
+        </ScrollView>
+      );
+    }
 
-        </View>
-      </View>
-    );
+    return (
+      <ScrollView style={styles.container}></ScrollView>
+    )
   }
 
 }
@@ -143,37 +199,50 @@ const styles = StyleSheet.create({
   },
   imageWrapper: {
     flex: 0.3,
+    marginTop: 5
   },
   inforWrapper: {
     flex: 0.7,
-    paddingHorizontal: 30,
   },
   titleWrapper: {
-    flex: 0.2,
+    flex: 0.3,
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+
   },
-  title:{
-    fontSize: 20, fontWeight: 'bold' 
+  sectionTitle: {
+    flex: 0.1, flexDirection: 'row', backgroundColor: '#ECE9F6', paddingVertical: 5,
+    paddingHorizontal: 30,
   },
-  producer:{
-    fontSize: 13, color: '#DEDEDE' 
+  title: {
+    fontSize: 20, fontWeight: 'bold'
   },
-  inventoryAmount:{
-    fontSize: 13, color: 'black' 
+  producer: {
+    fontSize: 13, color: '#DEDEDE'
+  },
+  inventoryAmount: {
+    fontSize: 13, color: 'black'
   },
   priceWrapper: {
     flex: 0.3,
-    paddingVertical: 25,
-  },
-  sectionTitle:{
-    flexDirection: 'row', backgroundColor: '#F5F5F5', paddingVertical: 5
+    paddingVertical: 15,
+    paddingHorizontal: 25,
   },
   peridodWrapper: {
-    flex: 0.5,
+    flex: 0.3,
+    paddingHorizontal: 30,
+
   }
 });
 
 DetailScreen.navigationOptions = {
   title: 'Detail Product',
-
+  headerStyle: {
+    backgroundColor: '#0AFAD5',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
 };
 
--- screens/InventoryScreen.js
+++ screens/InventoryScreen.js
@@ -1,30 +1,66 @@
 import React, { Component } from 'react';
 import { Container, Form, Picker, Icon, Content, List, ListItem, Thumbnail, Text, Left, Body, Right, Button } from 'native-base';
-import { StyleSheet, View, ProgressBarAndroid } from 'react-native';
-let inventoryProduct = [
+import { StyleSheet, View, ProgressBarAndroid, Image } from 'react-native';
+import EventEmitter from 'events'
+
+const INVENTORY_PRODUCT = [
   {
     'id': 1,
     'name': 'Bitis Hunter 2018',
-    'image': require('../assets/images/bitis_hunter_x_2019.jpg'),
-    'price': 900000
+    'image': require('../assets/images/bitis_hunter_x_2019_black.jpg'),
+    'price': '900.000',
+    'producer':'Bitis'
   },
   {
     'id': 2,
     'name': 'Bitis Hunter 2019',
     'image': require('../assets/images/bitis_hunter_x_2019.jpg'),
-    'price': 900000
+    'price': '900.000',
+    'price': 900000,
+    'producer':'Bitis'
+
   },
   {
     'id': 3,
     'name': 'Bitis Hunter 2020',
-    'image': require('../assets/images/bitis_hunter_x_2019.jpg'),
-    'price': 900000
+    'image': require('../assets/images/bitis_hunter_x_2019_orange.jpg'),
+    'price': '900.000',
+    'producer':'Bitis'
   },
   {
     'id': 4,
     'name': 'Bitis Hunter 2021',
-    'image':require('../assets/images/bitis_hunter_x_2019.jpg'),
-    'price': 900000
+    'image': require('../assets/images/bitis_hunter_x_2019_white.jpg'),
+    'price': '900.000',
+    'price': 900000,
+    'producer':'Bitis'
+  },
+  {
+    'id': 5,
+    'name': 'Bitis Hunter 2018',
+    'image': require('../assets/images/bitis_hunter_x_2019_black.jpg'),
+    'price': '900.000',
+    'producer':'Bitis'
+  },
+  {
+    'id': 6,
+    'name': 'Bitis Hunter 2019',
+    'image': require('../assets/images/bitis_hunter_x_2019.jpg'),
+    'producer':'Bitis'
+  },
+  {
+    'id': 7,
+    'name': 'Bitis Hunter 2020',
+    'image': require('../assets/images/bitis_hunter_x_2019_orange.jpg'),
+    'price': '900.000',
+    'producer':'Bitis'
+  },
+  {
+    'id': 8,
+    'name': 'Bitis Hunter 2021',
+    'image': require('../assets/images/bitis_hunter_x_2019_white.jpg'),
+    'price': '900.000',
+    'producer':'Bitis'
   },
 ]
 
@@ -33,68 +69,126 @@ export default class InventoryScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: undefined
+      inventoryProduct: [],
+      selectedCategory: undefined,
+      selectedPrice: undefined
     };
+
+    this.updateInventoryEvent = new EventEmitter()
+  }
+
+  componentWillMount() {
+
+    this.updateInventoryEvent.addListener("updateInventory", (inventory) => {
+      this.updateInventory(inventory)
+    })
+
+    this.setState({
+      inventoryProduct: INVENTORY_PRODUCT
+    })
+    console.log("this inventoryProduct" + this.state.inventoryProduct)
+  }
+
+  updateInventory = (_updatedInventory) => {
+    //for r update thui
+    if (_updatedInventory) {
+      let updatedInventory = INVENTORY_PRODUCT.filter((inventory) => {
+        return inventory.id == _updatedInventory.id
+      })
+
+      INVENTORY_PRODUCT.map(inventory => {
+        if (inventory.id == updatedInventory.id) {
+          inventory.price = updatedInventory.price
+        }
+      })
+
+      this.setState({ inventoryProduct: INVENTORY_PRODUCT })
+    }
+    // alert(JSON.stringify(_updatedInventory))
   }
 
   renderListInventory() {
-    return inventoryProduct.map((item) => {
-      return (
-        <ListItem thumbnail key={item.id} style={{ borderWidth: 0}}>
-          <View style={{ flex: 1, flexDirection: 'column' }} >
+    if (this.state.inventoryProduct) {
+      return this.state.inventoryProduct.map((item) => {
+        return (
+          <ListItem thumbnail key={item.id} style={{ borderWidth: 0 }}>
             <View style={{ flex: 1, flexDirection: 'row' }}>
               <Left>
-                <Thumbnail square source={item.image} />
+                <Thumbnail style={{width:80,height:80}} source={item.image} />
               </Left>
               <Body>
-                <Text>{item.name}</Text>
-                <Text note numberOfLines={1} style={{ color: 'green' }}>{item.price} VNĐ</Text>
+                <Text style={{ color: '#003459',fontSize: 16}}>{item.name}</Text>
+                <Text note numberOfLines={1} style={{ color: '#dedede',fontSize: 13 }}>{item.producer}</Text>
+                <Text note numberOfLines={1} style={{ color: '#20E347',fontSize: 16 }}>{item.price} VNĐ</Text>
+                <Text note numberOfLines={1} style={{ color: '#006494',fontSize: 13 }}>9/10 sản phẫm tồn</Text>
               </Body>
             </View>
-            <View style={{ flex: 1, backgroundColor: 'yellow', height: 10 }}>
-              <ProgressBarAndroid styleAttr="Horizontal" color="#2196F3" />
-            </View>
-          </View>
-          <Right  style={{ borderBottomWidth: 0}}>
-            <Button transparent onPress={() => this.props.navigation.navigate('Detail')}>
-              <Text>THAY ĐỔI GIÁ</Text>
-            </Button>
-          </Right>
-        </ListItem>
-      )
-    })
+            <Right style={{width:100}}>
+              <Button style = {{backgroundColor:'#E63946',borderRadius:30}} transparent onPress={() =>
+                this.props.navigation.navigate('Detail',
+                  {
+                    inventory: item,
+                    updateInventoryEvent: this.updateInventoryEvent
+                  })}>
+                <Text style= {{color:"white"}}>ĐỔI GIÁ</Text>
+              </Button>
+            </Right>
+          </ListItem>
+        )
+      })
+    }
+
+    return (
+      <View style={{ justifyContent: "center", alignItems: "center" }}>
+        <Text>Không có sản phẫm !!</Text>
+      </View>
+    );
   }
 
-  onValueChange(value: string) {
-    this.setState({
-      selected: value
-    });
+  onCategoryChange = (selectedCategory) => {
+    selectedCategory ? selectedCategory : null
+    if (selectedCategory != "all") {
+      let inventoryProductFilter = INVENTORY_PRODUCT.filter((inventory) => {
+        return inventory.name == selectedCategory
+      })
+      this.setState({ inventoryProduct: inventoryProductFilter, selectedCategory })
+    } else {
+      console.log("all")
+      this.setState({ inventoryProduct: INVENTORY_PRODUCT, selectedCategory: "all" })
+    }
+  }
+
+  onPriceChange = (value) => {
+
+    console.log("onPriceChange" + this.state.selectedPrice)
   }
 
   render() {
     return (
       <Container>
-
-        <Content>
-          <Form style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', paddingHorizontal: 20, backgroundColor: '#DEDEDE' }}>
-            <Icon name='funnel' />
+        <Content style={{ backgroundColor: '#DEDEDE' }}>
+          <Form style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', paddingHorizontal: 15, marginVertical: 10, backgroundColor: 'white' }}>
+            <Image style={{
+              width: 30,
+              height: 30,
+              backgroundColor: '#DEDEDE',
+              resizeMode: 'stretch'
+            }} source={require("../assets/images/filter.png")} />
             <Picker
               mode="dropdown"
-              selectedValue={this.state.selected}
-              onValueChange={this.onValueChange.bind(this)}
+              style={{ width: 70 }}
+              selectedValue={this.state.selectedCategory}
+              onValueChange={this.onCategoryChange}
             >
-              <Picker.Item label="Loại" value="key0" />
-              <Picker.Item label="ATM Card" value="key1" />
-              <Picker.Item label="Debit Card" value="key2" />
-              <Picker.Item label="Credit Card" value="key3" />
-              <Picker.Item label="Net Banking" value="key4" />
-              <Picker.Item label="Bitis Hunter X" value="key0" />
+              <Picker.Item label="Loại" value="all" />
+              <Picker.Item label="Bitis Hunter X 2018" value="Bitis Hunter 2018" />
+              <Picker.Item label="Bitis Hunter X 2019" value="Bitis Hunter 2019" />
+              <Picker.Item label="Bitis Hunter X 2020" value="Bitis Hunter 2020" />
             </Picker>
             <Picker
               mode="dropdown"
-              style={{ width: 50 }}
-              selectedValue={this.state.selected}
-              onValueChange={this.onValueChange.bind(this)}
+              selectedValue={this.state.selectedPrice}
+              onValueChange={this.onPriceChange}
             >
               <Picker.Item label="Số lượng" value="key0" />
               <Picker.Item label="ATM Card" value="key1" />
@@ -103,7 +197,7 @@ export default class InventoryScreen extends Component {
               <Picker.Item label="Net Banking" value="key4" />
             </Picker>
           </Form>
-          <List>
+          <List style={{ backgroundColor: 'white' }}>
             {this.renderListInventory()}
           </List>
         </Content>
@@ -113,6 +207,13 @@ export default class InventoryScreen extends Component {
 }
 
 InventoryScreen.navigationOptions = {
-  title: 'Inventory'
+  title: 'Inventory',
+  headerStyle: {
+    backgroundColor: '#0AFAD5',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
 };
 
